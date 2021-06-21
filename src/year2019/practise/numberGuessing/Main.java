24847c7bb06fc489fab72cf44a85355acee6832a
Jonas Schmucker <82109750+JonasSchmucker@users.noreply.github.com>
2021-06-22 01:53:05 +0200

    initial commit

---
+++ src/year2019/practise/numberGuessing/Main.java
@@ -0,0 +1,10 @@
+package year2019.practise.numberGuessing;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    }
+}
