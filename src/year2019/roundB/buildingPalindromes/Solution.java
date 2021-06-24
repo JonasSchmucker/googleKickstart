f28737982641f6bdd888365678687b26d8a8aac0
Jonas Schmucker <82109750+JonasSchmucker@users.noreply.github.com>
2021-06-24 17:25:24 +0200

    passed building Palindromes

--- src/year2019/roundB/buildingPalindromes/Solution.java
+++ src/year2019/roundB/buildingPalindromes/Solution.java
@@ -15,43 +15,38 @@ public class Solution {
         int n = 0;
         int questionCount = 0;
         String text;
-        int [] lowerBounds;
-        int [] upperBounds;
-        Thread [] threads;
+        Question [] questions;
         for(int testcase = 1; testcase < testcases + 1; testcase++) {
             n = sc.nextInt();
             questionCount = sc.nextInt();
             text = sc.next();
-            lowerBounds = new int[questionCount];
-            upperBounds = new int[questionCount];
-            threads = new Thread[questionCount];
+            questions = new Question[questionCount];
             for(int i = 0; i < questionCount; i++){
-                lowerBounds[i] = sc.nextInt();
-                upperBounds[i] = sc.nextInt();
+                questions[i] = new Question(sc.nextInt(), sc.nextInt());
             }
+
+            Arrays.sort(questions);
 
             answer = 0;
-
-            for(int i = 0; i < questionCount; i++){
-                int[] finalUpperBounds = upperBounds;
-                int finalI = i;
-                int[] finalLowerBounds = lowerBounds;
-                String finalText = text;
-                threads[i] = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        answer += isPalindromeMaterial(finalText.substring(finalLowerBounds[finalI] - 1, finalUpperBounds[finalI]))?1:0;
-                    }
-                });
-                threads[i].run();
-            }
-
-            for(int i = 0; i < questionCount; i++){
-                try {
-                    threads[i].join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            Arrays.fill(alphabet, 0);
+            int lastLowerBound = questions[0].lowerBound;
+            inputArray(text.substring(questions[0].lowerBound - 1, questions[0].upperBound));
+            answer += isPalindromeInArray()?1:0;
+            for(int i = 1; i < questionCount; i++){
+                if(lastLowerBound == questions[i].lowerBound){
+                    inputArray(text.substring(questions[i - 1].upperBound, questions[i].upperBound));
                 }
+                else{
+                    removeArray(text.substring(lastLowerBound - 1, questions[i].lowerBound - 1));
+                    if(questions[i].upperBound < questions[i - 1].upperBound){
+                        removeArray(text.substring(questions[i].upperBound, questions[i - 1].upperBound));
+                    }
+                    else{
+                        inputArray(text.substring(questions[i - 1].upperBound, questions[i].upperBound));
+                    }
+                    lastLowerBound = questions[i].lowerBound;
+                }
+                answer += isPalindromeInArray()?1:0;
             }
 
             printOutput(testcase, answer);
@@ -60,9 +55,11 @@ public class Solution {
 
     private static boolean isPalindromeMaterial(String substring) {
         Arrays.fill(alphabet, 0);
-        for(int i = 0; i < substring.length(); i++){
-            alphabet[substring.charAt(i) - 'A']++;
-        }
+        inputArray(substring);
+        return isPalindromeInArray();
+    }
+
+    private static boolean isPalindromeInArray() {
         boolean singleLetterJoker = true;
         for(int i = 0; i < alphabet.length; i++){
             if(alphabet[i]%2 == 1){
@@ -76,7 +73,38 @@ public class Solution {
         return true;
     }
 
+    private static void inputArray(String substring) {
+        for(int i = 0; i < substring.length(); i++){
+            alphabet[substring.charAt(i) - 'A']++;
+        }
+    }
+
+    private static  void removeArray(String substring){
+        for(int i = 0; i < substring.length(); i++){
+            alphabet[substring.charAt(i) - 'A']--;
+        }
+    }
+
     public static void printOutput(int testcase, int answer){
         System.out.println("Case #" + testcase +": "+ answer);
+    }
+
+    static class Question implements Comparable<Question>{
+        int lowerBound = 0;
+        int upperBound = 0;
+
+        public Question(int lowerBound, int upperBound) {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+        }
+
+        @Override
+        public int compareTo(Question o) {
+            return (lowerBound == o.lowerBound) ? upperBound - o.upperBound : lowerBound - o.lowerBound;
+        }
+
+        public String toString(){
+            return "From " + lowerBound + " to " + upperBound;
+        }
     }
 }
