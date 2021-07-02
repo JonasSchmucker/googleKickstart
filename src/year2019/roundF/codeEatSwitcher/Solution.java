bd92467645cede59c7f72ccce67ec045e2ebc739
Jonas Schmucker <82109750+JonasSchmucker@users.noreply.github.com>
2021-07-03 00:47:37 +0200

    tried codeEatSwitcher again

--- src/year2019/roundF/codeEatSwitcher/Solution.java
+++ src/year2019/roundF/codeEatSwitcher/Solution.java
@@ -2,7 +2,7 @@ package year2019.roundF.codeEatSwitcher;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     static Scanner sc;
@@ -14,41 +14,116 @@ public class Solution {
         int days = 0;
         int slotsPerDay = 0;
 
-        long maxCoding = 0;
-        long maxEating = 0;
-        long bigProduct = 0;
+        long [] coding = new long[10000];
+        long [] eating = new long[10000];
 
-        long [] minCoding = new long[10000];
-        long [] minEating = new long[10000];
+        Point [] minPoints = new Point[10000];
+
+        Map<Long, Point> codingToPoints = new HashMap<>();
+        TreeSet<Long> values = new TreeSet<>();
+        ArrayList<Long> toAdd = new ArrayList<>();
 
         for(int testcase = 1; testcase < testcases + 1; testcase++) {
             days = sc.nextInt();
             slotsPerDay = sc.nextInt();
+            codingToPoints.put(0L, new Point(0, 0));
+            values.add(0L);
 
             for(int i = 0; i < slotsPerDay; i++){
-                maxCoding += sc.nextInt();
-                maxEating += sc.nextInt();
+                coding[i] = sc.nextInt();
+                eating[i] = sc.nextInt();
             }
-            bigProduct = maxCoding * maxEating;
 
             for(int i = 0; i < days; i++){
-                minCoding[i] = sc.nextInt();
-                minEating[i] = sc.nextInt();
+                minPoints[i] = new Point(sc.nextInt(), sc.nextInt());
             }
 
-            // Graph for line: maxCoding - (maxCoding / maxEating) * x, Testcase 1: 9 - (9 / 18) * x => 9 - 0.5 * x
-            // Equation: maxCoding - maxCoding / maxEating * minEating >= minCoding, Testcase 1: 9 - (9 / 18) * 18 >= 0
-            // Simplification: maxCoding * maxEating - maxCoding * minEating >= minCoding * maxEating, Testcase 1: 9 * 18 - 9 * 18 >= 0 * 18
-            // Simplification: maxCoding * maxEating >= minCoding * maxEating + maxCoding * minEating , Testcase 1: 9 * 18 >= 0 * 18 + 9 * 18
+            for(int i = 0; i < slotsPerDay; i++){
+                for(Long thisKey : values){
+                    long thisEating = codingToPoints.get(thisKey).eating;
+                    toAdd.add(thisKey + coding[i]);
+                    codingToPoints.merge(thisKey + coding[i], new Point(thisKey + coding[i], thisEating), Solution::higherEating);
+                    codingToPoints.merge(thisKey, new Point(thisKey, thisEating + eating[i]), Solution::higherEating);
+                }
+
+                values.addAll(toAdd);
+                toAdd.clear();
+
+                int firstLast = 0;
+                for(Long thisValue : values){
+                    if(firstLast != 0 && firstLast != values.size() - 1){
+                        if(redundant(codingToPoints, values, codingToPoints.get(thisValue))){
+                            toAdd.add(thisValue);
+                        }
+                    }
+                    firstLast++;
+                }
+
+                values.removeAll(toAdd);
+                codingToPoints.keySet().removeAll(toAdd);
+                toAdd.clear();
+            }
+
             printOutput(testcase);
 
-            long right;
             for(int i = 0; i < days; i++){
-                right = minCoding[i] * maxEating + maxCoding * minEating[i];
-                System.out.print((bigProduct >= right) ?  "Y" : "N");
+                System.out.print(possible(codingToPoints, values, minPoints[i]) ? "Y": "N");
             }
 
+
             System.out.println();
+        }
+    }
+
+    private static boolean possible(Map<Long, Point> codingToPoints, TreeSet<Long> values, Point minPoint) {
+        long lowCoding = values.floor(minPoint.coding);
+        long lowEating = codingToPoints.get(lowCoding).eating;
+
+        if(values.last() == lowCoding){
+            return false;
+        }
+
+        long highCoding = values.ceiling(minPoint.coding);
+        long highEating = codingToPoints.get(highCoding).eating;
+
+        // (minPoint.coding - lowCoding)*((highEating - lowEating) / (highCoding - lowCoding)) + lowEating >= minPoint.eating
+        return (minPoint.coding - lowCoding)*(highEating - lowEating) + lowEating * (highCoding - lowCoding) >= minPoint.eating * (highCoding - lowCoding);
+    }
+
+    private static boolean redundant(Map<Long, Point> codingToPoints, TreeSet<Long> values, Point minPoint) {
+        long lowCoding = values.lower(minPoint.coding);
+        long lowEating = codingToPoints.get(lowCoding).eating;
+
+        if(values.last() == lowCoding){
+            return false;
+        }
+
+        long highCoding = values.higher(minPoint.coding);
+        long highEating = codingToPoints.get(highCoding).eating;
+
+        // (minPoint.coding - lowCoding)*((highEating - lowEating) / (highCoding - lowCoding)) + lowEating >= minPoint.eating
+        return (minPoint.coding - lowCoding)*(highEating - lowEating) + lowEating * (highCoding - lowCoding) >= minPoint.eating * (highCoding - lowCoding);
+    }
+
+    static Point higherEating(Point p1, Point p2){
+        return p2.eating > p1.eating ? p2 : p1;
+    }
+
+    static class Point{
+        long coding;
+        long eating;
+
+        public Point(long coding, long eating) {
+            this.eating = eating;
+            this.coding = coding;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "coding=" + coding +
+                    ", eating=" + eating +
+                    '}';
         }
     }
 
